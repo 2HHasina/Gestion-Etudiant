0afe786dad0fe4f07617ca95c619550796a2fa71
Amine Anbari <aanbari@e1r6p15.1337.ma>
2021-02-02 17:31:16 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
+import SignIn from "./components/auth/SignIn"
+import NavBar from "./components/Navbar/NavBar"
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Dashboard />
-      </div>
+      <SignIn />
+      
     </BrowserRouter>
   );
 }
---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import "../../style/Auth.css";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+class SignIn extends Component {
+  state = {
+    email: "",
+    password: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = () => {
+      console.log(this.state)
+  }
+
+  render() {
+
+    return (
+      <div className="content">
+        <Form
+          name="normal_login"
+          className="login-form"
+        onFinish={this.handleSubmit}
+        >
+          <div className="icon">
+            <i className="fas fa-user-lock"></i>
+          </div>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Email!",
+              },
+            ]}
+            onChange={this.handleChange}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+              name="email"
+              onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+            >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
+            </Button>
+            <div className="link">
+              Or
+              <br />
+              <a href="">Register now!</a>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
